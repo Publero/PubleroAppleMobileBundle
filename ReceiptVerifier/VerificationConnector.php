6af8161ed329c1fd7b776c7cc0ec6dc3ac47c1ab
mhlavac <info@mhlavac.net>
2012-07-19 21:56:06 +0200

    More refactoring

--- ReceiptVerifier/VerificationConnector.php
+++ ReceiptVerifier/VerificationConnector.php
@@ -1,17 +1,42 @@
 <?php
+namespace Publero\AppleMobileBundle\ReceiptVerifier;
 
 class VerificationConnector
 {
     /**
+     * @var string
+     */
+    private $verificationUrl;
+
+    /**
+     * @param string $verificationUrl
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($verificationUrl)
+    {
+        if (empty($verificationUrl)) {
+            throw new \InvalidArgumentException('Verification URL can not be empty');
+        }
+
+        $this->verificationUrl = $verificationUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVerificationUrl()
+    {
+        return $this->verificationUrl;
+    }
+
+    /**
      * @param string $receipt
+     * @throws \RuntimeException
      * @return array
      */
     public function makeRequest($receipt)
     {
-        $curl = curl_init($this->getVerificationUrl());
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+        $curl = $this->initCurl($receipt);
 
         $response     = curl_exec($curl);
         $errorNumber  = curl_errno($curl);
@@ -22,7 +47,22 @@ class VerificationConnector
             throw new \RuntimeException($errorMessage, $errorNumber);
         }
 
-        return $response;
+        return $this->decodeResponse($response);
+    }
+
+    /**
+     * @param string $receipt
+     * @return resource curl
+     */
+    private function initCurl($receipt)
+    {
+        $curl = curl_init($this->getVerificationUrl());
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->prepareReceipt($receipt));
+
+        return $curl;
     }
 
     /**
@@ -32,5 +72,14 @@ class VerificationConnector
     private function prepareReceipt($receipt)
     {
         return json_encode(array('receipt-data' => $receipt));
+    }
+
+    /**
+     * @param string $response
+     * @return array
+     */
+    private function decodeResponse($response)
+    {
+        return json_decode($response);
     }
 }
